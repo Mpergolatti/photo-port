3b8b3076eac7f64aff974f5aa71d0fb5234e9f11
Mpergolatti <Mpergolatti@gmail.com>
2022-12-06 22:03:04 -0500

    About and Nav section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
-    
     <div>
-
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
-  )
+  );
 }
 
 export default App;
