e01ba5cc618aee36eaabf53e7c5b12e7720db266
Mpergolatti <Mpergolatti@gmail.com>
2022-12-10 12:05:44 -0500

    testing issues with Contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
+
 function App() {
+
   const [categories] = useState ([
 
     { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
@@ -15,6 +17,8 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
   
   return (
     <div>
@@ -22,13 +26,18 @@ function App() {
         categories={categories} 
         setCurrentCategory={setCurrentCategory} 
         currentCategory={currentCategory}
-      />
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
-        <ContactForm />
-        <Gallery
-          currentCategory={currentCategory}
-        />
-        <About />
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>  
+        )}
       </main>
     </div>
   );
